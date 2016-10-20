c8317034bf739bab83f8b27fe5fdc642ca651647
anuradha1994 <anuradhakulkarni94@gmail.com>
2016-10-20 01:35:03 -0700

    CollabrationProject-Backend
    It contains user,forum,blog,event model classes,DAO and DAO Impl.

--- ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/util/Util.java
+++
@@ -1,9 +0,0 @@
-package com.niit.shoppingcart.util;
-
-public class Util {
-	public static String removeComma(String name){
-		//REPLACE , WITH <EMPTYSPACE>
-		return name.replace(",", "");
-	}
-	
-}
